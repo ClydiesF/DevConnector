d9a98a71d9618c42fd6f41e3671e3cb1c13dd55a
Clydies Freeman <clyde@joinsoko.com>
2020-11-15 22:31:22 -0600

    added profile route details

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }, 
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+// Here we are exporting a variabel that takes in the model with th model name and schema
+
+module.exports = User = mongoose.model('user', UserSchema)
